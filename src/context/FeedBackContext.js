933d8fc3c94e4200bc44f9c6f705dca869e1bf96
ozan isgor <ozanisgor11@gmail.com>
2022-12-22 13:00:17 +0300

    add: addFeedback & proxy

--- src/context/FeedBackContext.js
+++ src/context/FeedBackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useEffect, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
@@ -18,7 +17,7 @@ export const FeedBackProvider = ({ children }) => {
 
   // Fetch feedback
   const fetchFeedback = async () => {
-    const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`);
+    const response = await fetch(`/feedback?_sort=id&_order=desc`);
     const data = await response.json();
     setFeedback(data);
     setIsLoading(false);
@@ -30,10 +29,17 @@ export const FeedBackProvider = ({ children }) => {
     }
   };
 
-  const addFeedback = newFeedback => {
-    newFeedback.id = uuidv4();
-    // newFeedback.id = parseInt(uuidv4());
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async newFeedback => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newFeedback)
+    });
+    const data = await response.json();
+
+    setFeedback([data, ...feedback]);
   };
 
   // update feedback item
