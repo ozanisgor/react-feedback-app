b2cbebd9653b056ddd5fde24a7472f30bc068787
ozan isgor <ozanisgor11@gmail.com>
2022-12-22 13:17:59 +0300

    add: delete from JSON-Server

--- src/context/FeedBackContext.js
+++ src/context/FeedBackContext.js
@@ -45,8 +45,16 @@ export const FeedBackProvider = ({ children }) => {
   };
 
   // update feedback item
-  const updateFeedback = (id, updItem) => {
-    setFeedback(feedback.map(item => (item.id === id ? { ...item, ...updItem } : item)));
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updItem)
+    });
+    const data = await response.json();
+    setFeedback(feedback.map(item => (item.id === id ? { ...item, ...data } : item)));
   };
 
   // set item to updated
